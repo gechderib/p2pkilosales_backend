5fc85a038fcedb82c511b8cabd47516b4e6ca4fc
gechderib <gechderib@gmail.com>
2026-01-12 15:59:50 +0300

    adding a standart response

--- config/views.py
+++ config/views.py
@@ -5,6 +5,19 @@ class StandardResponseViewSet(viewsets.ModelViewSet):
     """
     Base ViewSet that provides standardized response format
     """
+    def _standardize_response(self, response):
+        """
+        Helper method to standardize a response object
+        """
+        if hasattr(response, 'data') and isinstance(response.data, dict):
+            if all(k in response.data for k in ["status", "data", "status_code", "error"]):
+                return response
+
+        return standard_response(
+            data=response.data if hasattr(response, 'data') else {},
+            status_code=response.status_code
+        )
+
     def finalize_response(self, request, response, *args, **kwargs):
         # Check if response is already standardized to avoid double wrapping
         if hasattr(response, 'data') and isinstance(response.data, dict):
@@ -22,6 +35,19 @@ class StandardAPIView(viewsets.views.APIView):
     """
     Base APIView that provides standardized response format
     """
+    def _standardize_response(self, response):
+        """
+        Helper method to standardize a response object
+        """
+        if hasattr(response, 'data') and isinstance(response.data, dict):
+            if all(k in response.data for k in ["status", "data", "status_code", "error"]):
+                return response
+
+        return standard_response(
+            data=response.data if hasattr(response, 'data') else {},
+            status_code=response.status_code
+        )
+
     def finalize_response(self, request, response, *args, **kwargs):
         if hasattr(response, 'data'):
             response = standard_response(
